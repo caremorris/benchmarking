9e211d44c56f79477b1bd11ec46e06ebdf1d9326
caremorris <morris.carolyn.a@gmail.com>
2014-05-28 15:34:50 -0400

    create dbs, execute queries, time it

--- benchmark.py
+++ benchmark.py
@@ -18,9 +18,8 @@ from influxdb import client as _influxClient
 import influxdb
 
 import tempodb
-#from tempodb import client
-#from tempodb.protocol import DataPoint
-from tempodb import Client, DataPoint
+from tempodb import client
+from tempodb.protocol import DataPoint
 
 import timeit
 
@@ -38,29 +37,24 @@ class CLDatabase(object):
     def insert_range(self, points_array):
         for i in points_array:
             print(i.time, i.value)
-    def select_range(self, first, last):
-        print("not implemented")
     def select_first(self):
         print("first")
     def select_last(self):
         print("last")
+    def select_range(self, first, last):
+        print("not implemented")
         
 class CLSQLite(CLDatabase):
     def __init__(self):
         conn = ''
         c = ''
-        #this should delete the db if it exists
-        #or connect to the db and delete the pts if there are any
+        # I tried to setup the connection & cursor here, and delete them from the next 4 methods,
+        # but resulted in "NameError: global name 'c' is not defined"
 
     def insert_range(self, points_array):
         conn = sqlite3.connect("grapes.db")
         c = conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS 'points' ('time' INTEGER, 'value' INTEGER)''')
-        #i = 0
-        #while i < 5:
-            #z = (points[i].time , points[i].value)
-            #c.execute('INSERT INTO points (time, value) VALUES (?, ?)', z)
-            #i = i + 1
         for p in points_array:
             z = (p.time , p.value)
             c.execute('INSERT INTO points (time, value) VALUES (?, ?)', z)
@@ -68,18 +62,10 @@ class CLSQLite(CLDatabase):
         conn.commit()
         conn.close()
 
-    def select_range(self, first, last):
-        conn = sqlite3.connect("grapes.db")
-        c = conn.cursor()
-        first = c.execute('SELECT * FROM points WHERE time > ' + first + ' and time < ' + last)
-
-        conn.commit()
-        conn.close()
-
     def select_first(self):
         conn = sqlite3.connect("grapes.db")
         c = conn.cursor()
-        first = c.execute('''SELECT * FROM points ORDER BY time ASC LIMIT 1''')
+        c.execute('''SELECT * FROM points ORDER BY time ASC LIMIT 1''')
 
         conn.commit()
         conn.close()
@@ -87,12 +73,22 @@ class CLSQLite(CLDatabase):
     def select_last(self):
         conn = sqlite3.connect("grapes.db")
         c = conn.cursor()
-        first = c.execute('''SELECT * FROM points ORDER BY time DESC LIMIT 1''')
+        c.execute('''SELECT * FROM points ORDER BY time DESC LIMIT 1''')
 
         conn.commit()
-        conn.close()       
+        conn.close()
+
+    def select_range(self, first, last):
+        conn = sqlite3.connect("grapes.db")
+        c = conn.cursor()
+        c.execute('SELECT * FROM points WHERE time > ' + first + ' and time < ' + last)
+
+        conn.commit()
+        conn.close()
 
 class CLInflux(CLDatabase):
+    # ordered desc by default
+    # time in microseconds (1000 milliseconds)
     _influxClient = influxdb.InfluxDBClient()
     def __init__(self):
         host = 'sandbox.influxdb.com'
@@ -114,6 +110,15 @@ class CLInflux(CLDatabase):
                 }]
             
         self._influxClient.write_points(data)
+
+    def select_first(self):
+        self._influxClient.query('select * from danjou order asc limit 1')
+
+    def select_last(self):
+        self._influxClient.query('select * from danjou limit 1')
+
+    def select_range(self, first, last):
+        self._influxClient.query('select * from json_points where time > '+first+' and time < '+last+'')
        
 class CLTempo(CLDatabase):
     # some private vars
@@ -125,10 +130,10 @@ class CLTempo(CLDatabase):
     
     def __init__(self):
         # get api key, secret, connect to tempo server, create series
-        self.API_KEY = '444011c85ac2426fb5f9bebc52d13298'
-        self.API_SECRET = '410c22c673584f568e01ac471d5570dc'
-        self.DATABASE_ID = 'dev-test'
-        self.SERIES_KEY = 'fruit'
+        self.API_KEY = '70cedcd39a304c30a61607e994d8a1b1'
+        self.API_SECRET = 'df900c9bb67a4fffa9127c548599f7e6'
+        self.DATABASE_ID = 'fruit'
+        self.SERIES_KEY = 'apple'
         self._client = tempodb.client.Client(self.DATABASE_ID, self.API_KEY, self.API_SECRET)
         # delete series if it's already there.
         self._client.delete_series(self.SERIES_KEY)
@@ -144,10 +149,18 @@ class CLTempo(CLDatabase):
             data.append(tempoPt)
 
         # write points out    
-        resp = self._client.write_data(self.SERIES_KEY, data)
-        print ('Temo Response code: ', resp.status)
-    
+        resp = self._client.write_key(self.SERIES_KEY, data)
 
+    def select_first(self):
+        self._client.single_value(self.SERIES_KEY, datetime.datetime(1950, 1, 1), 'nearest')
+        
+    def select_last(self):
+        self._client.single_value(self.SERIES_KEY, datetime.datetime(2050, 1, 1), 'nearest')
+
+    def select_range(self, first, last):
+        # time format ISO 8601
+        self._client.read_data(self.SERIES_KEY, first, last)
+    
 
 ########################
 ####  TESTING CODE  ####
@@ -163,18 +176,26 @@ for i in range(100):
     points.append(Point(timestamp, random.randint(1,100)))
     theDate = theDate + datetime.timedelta(minutes=5)
 
+sqlite = CLSQLite()
+sqlite.insert_range(points)
+
+influx = CLInflux()
+#influx.insert_range(points)
+
+tempo = CLTempo()
+#tempo.insert_range(points)
+
+# utc timestamp
 first = '1388535600'
 last = '1388537100'
-sqlite = CLSQLite()
-#sqlite.insert_range(points)
+
+# ISO 8601
+start = '2014-01-01T00:00:00Z'
+end = '2014-01-01T03:00:00Z'
+
 tic = time.time()
-sqlite.select_range(first, last)
+tempo.select_range(start, end)
 toc = time.time()
 print(toc-tic)
 
 
-#influx = CLInflux()
-#influx.insert_range(points)
-
-#tempo = CLTempo()
-#tempo.insert_range(points)
